95e436eb496f427f5ed4675e3d3dac5084928507
tesfa373 <t76189857@gmail.com>
2025-08-18 23:19:06 +0300

    Add task_2.sql with all tables for alx_book_store

---
+++ task_2.sql
@@ -0,0 +1,44 @@
+-- Use the database
+USE alx_book_store;
+
+-- Authors table
+CREATE TABLE IF NOT EXISTS authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+-- Books table
+CREATE TABLE IF NOT EXISTS books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
+
+-- Customers table
+CREATE TABLE IF NOT EXISTS customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+-- Orders table
+CREATE TABLE IF NOT EXISTS orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- Order Details table
+CREATE TABLE IF NOT EXISTS order_details (
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
