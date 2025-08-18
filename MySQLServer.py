b44877203b08e5c67cecc6339a9eaab3f5da0c57
tesfa373 <t76189857@gmail.com>
2025-08-18 23:04:37 +0300

    Fix: use except mysql.connector.Error for checker

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,20 +1,19 @@
 import mysql.connector
-from mysql.connector import Error
 
 try:
+    # Connect to MySQL server
     connection = mysql.connector.connect(
         host='localhost',
-        user='root',
-        password='yourpassword'
+        user='root',            # replace with your MySQL username
+        password='yourpassword' # replace with your MySQL password
     )
 
     if connection.is_connected():
         cursor = connection.cursor()
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-
         print("Database 'alx_book_store' created successfully!")
 
-except Error as e:
+except mysql.connector.Error as e:
     print("Error while connecting to MySQL:", e)
 
 finally:
